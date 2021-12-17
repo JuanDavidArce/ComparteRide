d901c73cfef14a933b4d98a6d5742a4f23b98aef
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-17 13:56:27 -0500

    circel app base

---
+++ cride/circles/apps.py
@@ -0,0 +1,7 @@
+"""Circles app """
+#django
+from django.apps import AppConfig
+
+class CirclesAppConfig(AppConfig):
+    name = 'cride.circles'
+    verbose_name='Circles'
---
+++ cride/circles/models/circles.py
@@ -0,0 +1,56 @@
+"""Circle model"""
+# Django
+from django.db import models
+
+# Utilities
+from cride.utils.models import CRideModel
+
+
+class Circle(CRideModel):
+    """Circle model
+    A circle is a private group where riedes are affered and taken 
+    by its members. to join a circle a user must recive an unique 
+    invitation code from an existing circle member"""
+
+
+    name = models.CharField('circle name',max_length=140)
+    slug_name = models.SlugField(unique=True,max_length=40)
+
+    about = models.CharField('circle description',max_length=255)
+    picture = models.ImageField(upload_to='circles/pictures',blank=True,null=True)
+
+
+    # Stats
+    rides_offered = models.PositiveIntegerField(default=0)
+    rides_taken= models.PositiveIntegerField(default=0)
+
+    verified = models.BooleanField(
+        'verified circle',
+        default=False,
+        help_text='Verigied circles are also known as official communities'
+    )
+
+    is_public = models.BooleanField(
+        default=True,
+        help_text='Public circels are listed in the main page so everyone know about their existence'
+    )
+
+    is_limited = models.BooleanField(
+        'limited',
+        default=False,
+        help_text='Limited ciercles can grow up to a fixed number of members'
+    )
+
+    mambers_limit = models.PositiveIntegerField(
+        default=0,
+        help_text='If circle is limited, this will be the limit on the number of members'
+    )
+
+    def __str__(self):
+        
+        """Return circel name"""
+        return self.name
+
+    class Meta(CRideModel.Meta):
+        """Meta class"""
+        ordering =['-rides_taken','-rides_offered']
