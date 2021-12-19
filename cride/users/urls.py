98863f6ec88d110ff4ee61baf916539e35e151a2
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-19 18:48:13 -0500

    LoginView

---
+++ cride/users/urls.py
@@ -0,0 +1,11 @@
+"""Users URLs"""
+
+# django
+from django.urls import path
+
+# Views
+from cride.users.views import UserLoginAPIView
+
+urlpatterns  = [
+    path('users/login/',UserLoginAPIView.as_view(),name='login'),
+]
