b2c8cce5c361589848d6616b7efc62f5e5a36dca
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-20 12:45:45 -0500

    SignUp view

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 
 # Serializers
-from cride.users.serializers import UserLoginSerializer,UserModelSerializer
+from cride.users.serializers import UserLoginSerializer,UserModelSerializer,UserSignUpSerializer
 
 
 class UserLoginAPIView(APIView):
@@ -22,4 +22,15 @@ class UserLoginAPIView(APIView):
             'user':UserModelSerializer(user).data,
             'access_token':token
         }
+        return Response(data,status = status.HTTP_201_CREATED)
+
+class UserSignUpAPIView(APIView):
+    """User SignUp API View"""
+
+    def post(self,request,*args,**kwargs):
+        """Handle HTTP POST request"""
+        serializer = UserSignUpSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        user= serializer.save()
+        data = UserModelSerializer(user).data
         return Response(data,status = status.HTTP_201_CREATED)
