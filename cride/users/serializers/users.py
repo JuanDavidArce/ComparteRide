21fc9c3eaed402cc10ac219afc0106299b5dc0e4
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-21 19:23:42 -0500

    Generating tokens and its expiration

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate,password_validation
 from django.core.validators import RegexValidator
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils import timezone
+from django.conf import settings
 
 # Django REST Framework
 from rest_framework import serializers
@@ -12,6 +14,10 @@ from rest_framework.validators import UniqueValidator
 
 # Models
 from cride.users.models import User,Profile
+
+# Utilities
+from datetime import timedelta
+import jwt
 
 
 class UserModelSerializer(serializers.ModelSerializer):
@@ -91,7 +97,17 @@ class UserSignUpSerializer(serializers.Serializer):
 
     def gen_verification_token(self,user):
         """Create JWT token that the user can use to verify ist account"""
-        return "abc"
+        exp_date = timezone.now() + timedelta(days=3)
+        payload = {
+            'user':user.username,
+            'exp':int(exp_date.timestamp()),
+            'type':'email_confirmation',
+
+        }
+
+        token = jwt.encode(payload, settings.SECRET_KEY,algorithm='HS256')
+        return token.decode()
+
 
 class UserLoginSerializer(serializers.Serializer):
     """User Login serializer.
