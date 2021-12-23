3f8fa54b7386640ddd5c7705df0f3d1e38cccad7
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-23 12:39:25 -0500

    Permissions

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,32 +1,50 @@
 """Circles views """
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
+
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
 
 
 # Models
-from cride.circles.models import Circle,Membership
+from cride.circles.models import Circle, Membership
 
 
-class CircleViewSet(viewsets.ModelViewSet):
+# Permissions
+from cride.circles.permissions.circles import IsCircleAdmin
+
+
+class CircleViewSet(mixins.CreateModelMixin, 
+                    mixins.RetrieveModelMixin, 
+                    mixins.ListModelMixin, 
+                    mixins.UpdateModelMixin, 
+                    viewsets.GenericViewSet):
+
     """Circle view set"""
     serializer_class = CircleModelSerializer
-    permission_classes = [IsAuthenticated]
 
-    def  get_queryset(self):
+
+    def get_permissions(self):
+        """Assing permissions based on action"""
+        permissions = [IsAuthenticated]
+        if self.action in ['update','partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
+
+    def get_queryset(self):
         """Restrict list to public-only"""
         queryset = Circle.objects.all()
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         """Assign circle admin"""
-        circle= serializer.save()
+        circle = serializer.save()
         user = self.request.user
         profile = user.profile
 
@@ -36,4 +54,4 @@ class CircleViewSet(viewsets.ModelViewSet):
             circle=circle,
             is_admin=True,
             remaining_invitations=10
-            )
+        )
