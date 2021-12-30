edba098a9990a805f72204f575a375f62a102566
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-30 11:38:19 -0500

    Admin actions, modifing data from a qyery

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -1,7 +1,6 @@
 """Circles admin"""
 # Django
 from django.contrib import admin
-from django.contrib.admin.options import ModelAdmin
 
 # Models
 from cride.circles.models import Circle
@@ -12,3 +11,18 @@ class CircleAdmin(admin.ModelAdmin):
     list_display =('slug_name','name','is_public','verified','is_limited','members_limit')
     search_fields= ('slug_name','name')
     list_filter =('is_public','verified','is_limited')
+
+
+    actions=['make_verified','make_unverified']
+
+
+    def make_verified(self,request,queryset):
+        """Make circles verified"""
+        queryset.update(verified=True)
+    
+    make_verified.short_description = 'Make selected circles verifided'
+    def make_unverified(self,request,queryset):
+        """Make circles unverified"""
+        queryset.update(verified=False)
+    
+    make_unverified.short_description = 'Make selected circles unverifided'
